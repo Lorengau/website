4c2920f27488f8c83da8077ece3136a5b8ae8e24
Martin <findmemart@gmail.com>
2022-10-12 19:37:23 +0200

    Sign Up Form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get "professional", to: "professional#index"
   get "private", to: "private#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
